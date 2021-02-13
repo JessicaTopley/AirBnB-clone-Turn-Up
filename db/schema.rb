dcada3355b31f17e0ce099f8d47755d519d674f1
Jessica Topley <jesse.topley@hotmail.com>
2021-02-13 10:40:37 +0000

    adding the seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_212716) do
+ActiveRecord::Schema.define(version: 2021_02_11_210811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_02_09_212716) do
     t.integer "occupancy"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 2021_02_09_212716) do
 
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
+  add_foreign_key "flats", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.destroy_all 
+User.destroy_all
+
+user1 = User.create!(email: "jessica@gmail.com", password: "123456", first_name: "Jessica", last_name: "Topley")
+5.times do
+  Flat.create(
+    name: ["Lovely flat in #{Faker::Address.city}", "Flat on #{Faker::Address.street_name}"].sample,
+    address: Faker::Address.full_address,
+    description: "Great flat with #{rand(3..6)} rooms, #{Faker::House.room}, #{Faker::House.room} and #{Faker::House.room}. Fully furnished.",
+    occupancy: rand(1..8),
+    user: user1
+  )
+end
+
+user2 = User.create!(email: "miranda@gmail.com", password: "123456", first_name: "Miranda", last_name: "Frudd")
+5.times do
+  Flat.create(
+    name: ["Lovely flat in #{Faker::Address.city}", "Flat on #{Faker::Address.street_name}"].sample,
+    address: Faker::Address.full_address,
+    description: "Great flat with #{rand(3..6)} rooms, #{Faker::House.room}, #{Faker::House.room} and #{Faker::House.room}. Fully furnished.",
+    occupancy: rand(1..8),
+    user: user2
+  )
+end
+
+user3 = User.create!(email: "catherine@gmail.com", password: "123456", first_name: "Catherine", last_name: "Maclellan")
+5.times do
+  Flat.create(
+    name: ["Lovely flat in #{Faker::Address.city}", "Flat on #{Faker::Address.street_name}"].sample,
+    address: Faker::Address.full_address,
+    description: "Great flat with #{rand(3..6)} rooms, #{Faker::House.room}, #{Faker::House.room} and #{Faker::House.room}. Fully furnished.",
+    occupancy: rand(1..8),
+    user: user3
+  )
+end
+
+user4 = User.create!(email: "martin@gmail.com", password: "123456", first_name: "Martin", last_name: "Mwaka")
+5.times do
+  Flat.create(
+    name: ["Lovely flat in #{Faker::Address.city}", "Flat on #{Faker::Address.street_name}"].sample,
+    address: Faker::Address.full_address,
+    description: "Great flat with #{rand(3..6)} rooms, #{Faker::House.room}, #{Faker::House.room} and #{Faker::House.room}. Fully furnished.",
+    occupancy: rand(1..8),
+    user: user4
+  )
+end
+
+  puts "#{Flat.count} flats created"
+
