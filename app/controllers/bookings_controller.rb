e12068bbd06cce9b984c306c6f3ef8b2ff9e81bd
Temceo <martinmwaka@hotmail.com>
2021-02-13 11:27:02 +0000

    added edit button

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,11 +8,8 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @flat = Flat.find(params[:flat_id])
 
-    # raise
     @booking.user = current_user
     @booking.flat = @flat
-    raise
-    # @booking.user_id?
     if @booking.save
       redirect_to flat_path(@flat)
     else
